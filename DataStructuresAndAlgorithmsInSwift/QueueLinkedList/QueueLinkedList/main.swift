9dc7f73d1d5ecae8e9c92b845671f690e48cb8e9
Jason Kim <active_red@hotmail.com>
2014-06-06 21:21:55 -0700

    initial q

---
+++ DataStructuresAndAlgorithmsInSwift/QueueLinkedList/QueueLinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  QueueLinkedList
+//
+//  Created by Jason Kim on 2014-06-06.
+//  Copyright (c) 2014 Jason Kim. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
